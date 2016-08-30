e00ab6375f9e73da815306e0767e2d6817cb29d6
Drew Samsen <drewsamsen@gmail.com>
2016-08-30 16:20:50 -0700

    = cleanup

--- src/components/common/api.js
+++ src/components/common/api.js
@@ -15,8 +15,6 @@ module.exports = {
     }
   },
 
-  // curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"email":"drewsamsen@gmail.com","password":"85Reccosos37qto"}' https://glacial-mountain-4423.herokuapp.com/api/login
-
   auth: {
     logIn: function(username, password, callbacks) {
       let path = `${rootUrl}/api/login`;
